af0774893e53bdcbce57d5aa59cdc2be80ba0fd2
IagoChaves <iagovieirachaves@gmail.com>
2020-04-14 04:09:19 -0300

    Challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let inc = 0;
+    let out = 0;
+    this.transactions.forEach(transact => {
+      if (transact.type === 'income') {
+        inc += transact.value;
+      } else {
+        out += transact.value;
+      }
+    });
+    const ac = inc - out;
+    return {
+      income: inc,
+      outcome: out,
+      total: ac,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ value, type, title }: CreateTransaction): Transaction {
+    const transaction = new Transaction({
+      title,
+      type,
+      value,
+    });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
